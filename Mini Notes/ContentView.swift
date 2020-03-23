fdead5388b9c5b7db8f259201c695789d14366bd
Sanju S <spikeysanju98@gmail.com>
2020-03-23 14:29:01 +0530

    Initial Commit

---
+++ Mini Notes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mini Notes
+//
+//  Created by Sanju S on 23/03/20.
+//  Copyright © 2020 Whizz App Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
